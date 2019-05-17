d49d47e320a69911a9eed12dd0217cde263bf907
Vast <Vastlee@gmail.com>
2019-05-17 14:19:30 -0700

    Started & Solved Reverse Words in a String III

---
+++ 557. Reverse Words in a String III/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_Words_in_a_String_III {
+  internal static class Solution {
+    internal static string ReverseWords(string s) {
+      char[] result = new char[s.Length];
+      int wordStartIndex;
+      bool spaceBreak = true;
+
+      wordStartIndex = 0;
+      for(int i = 0; i < result.Length; i++) {
+        if(spaceBreak) {
+          wordStartIndex = i;
+          spaceBreak = false;
+        }
+
+        if(s[i] == ' ') {
+          ReverseToFrom(result, ref s, wordStartIndex, i - 1);
+          result[i] = ' ';
+          spaceBreak = true;
+        }
+
+        if(i == result.Length - 1) {
+          ReverseToFrom(result, ref s, wordStartIndex, i);
+        }
+      }
+
+      return new string(result);
+    }
+
+    private static void ReverseToFrom(char[] arr, ref string s, int start, int end) {
+      for(int i = start, i2 = end; i <= end; i++, i2--) {
+        arr[i] = s[i2];
+      }
+    }
+
+    static void Main(string[] args) => Console.WriteLine(ReverseWords("Hello World!"));
+  }
+}
+
