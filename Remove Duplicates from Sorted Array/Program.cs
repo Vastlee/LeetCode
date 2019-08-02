e5376fcf59d8d4cd968899148fe95b19da24a712
Vast <Vastlee@gmail.com>
2019-08-01 17:44:32 -0700

    Added Solution & Solve

--- Remove Duplicates from Sorted Array/Program.cs
+++ Remove Duplicates from Sorted Array/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 
-namespace Remove_Duplicates_from_Sorted_Array
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Remove_Duplicates_from_Sorted_Array {
+    class Program {
+        static void Main(string[] args) {
+            int result = Solution.RemoveDuplicates(new int[] { 1, 1, 1, 2, 3, 4 });
+            Console.WriteLine(result.ToString());
         }
     }
 }
--- Remove Duplicates from Sorted Array/Solution.cs
+++ Remove Duplicates from Sorted Array/Solution.cs
@@ -1,5 +1,15 @@
-namespace Remove_Duplicates_from_Sorted_Array {
-    public class Solution {
+using System.Collections.Generic;
 
+namespace Remove_Duplicates_from_Sorted_Array {
+    public static class Solution {
+        public static int RemoveDuplicates(int[] nums) {
+            var set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++) {
+                if (!set.Contains(nums[i])) {
+                    set.Add(nums[i]);
+                }
+            }
+            return set.Count;
+        }
     }
 }
