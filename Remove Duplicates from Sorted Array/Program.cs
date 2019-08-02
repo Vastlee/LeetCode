ac67a02ed395aa4cc093d751ffcc58d57a95831b
Vast <Vastlee@gmail.com>
2019-08-01 22:41:03 -0700

    Added Solution for Remove Duplicates Sorted Array

--- Remove Duplicates from Sorted Array/Program.cs
+++ Remove Duplicates from Sorted Array/Program.cs
@@ -3,8 +3,16 @@
 namespace Remove_Duplicates_from_Sorted_Array {
     class Program {
         static void Main(string[] args) {
-            int result = Solution.RemoveDuplicates(new int[] { 1, 1, 1, 2, 3, 4 });
-            Console.WriteLine(result.ToString());
+            var solution = new Solution();
+
+            int[] input = new int[] { 1, 1, 1, 2, 3, 4 };
+            int result = solution.RemoveDuplicates(input);
+            for (int i = 0; i < input.Length; i++) {
+                Console.Write(input[i]);
+                if (i < input.Length) { Console.Write(", "); }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Count: {result.ToString()}");
         }
     }
 }
--- Remove Duplicates from Sorted Array/Solution.cs
+++ Remove Duplicates from Sorted Array/Solution.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections.Generic;
 
 namespace Remove_Duplicates_from_Sorted_Array {
-    public static class Solution {
-        public static int RemoveDuplicates(int[] nums) {
+    public class Solution {
+        public int RemoveDuplicates(int[] nums) {
             var set = new HashSet<int>();
+            int lastGoodIndex = 0;
             for (int i = 0; i < nums.Length; i++) {
                 if (!set.Contains(nums[i])) {
                     set.Add(nums[i]);
+                    nums[lastGoodIndex++] = nums[i];
                 }
             }
-            return set.Count;
+            return lastGoodIndex;
         }
     }
 }
