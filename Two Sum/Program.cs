3688085216a606759e76d7e5feaa87dd77386373
Vast <Vastlee@gmail.com>
2019-08-18 06:07:02 -0700

    Removed Common Updated some more VastTools

--- Two Sum/Program.cs
+++ Two Sum/Program.cs
@@ -1,11 +1,11 @@
-﻿using Common;
+﻿using VastTools;
 
 namespace Two_Sum {
   internal static class Program {
     private static void Main(string[] args) {
       int[] nums = new int[] { 2, 7, 11, 15 };
       int target = 9;
-      new Solution().TwoSum(nums, target).PrintArray<int>();
+      new Solution().TwoSum(nums, target).Log();
     }
   }
 }
