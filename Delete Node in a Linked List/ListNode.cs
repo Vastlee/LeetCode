c1420fe80d7e3815639476f723c331b86728b34f
Vast <Vastlee@gmail.com>
2019-08-18 06:49:47 -0700

    Added Delete Node in a Linked List Project & Solve

---
+++ Delete Node in a Linked List/ListNode.cs
@@ -0,0 +1,9 @@
+﻿namespace Delete_Node_in_a_Linked_List {
+  public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+      val = x;
+    }
+  }
+}
---
+++ Delete Node in a Linked List/Solution.cs
@@ -0,0 +1,8 @@
+﻿namespace Delete_Node_in_a_Linked_List {
+  internal class Solution {
+    public void DeleteNode(ListNode node) {
+      node.val = node.next.val;
+      node.next = node.next.next;
+    }
+  }
+}
