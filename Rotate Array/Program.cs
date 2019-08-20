95794215525b2c6e3375cd61f56765b1aa03ca9c
Vast <Vastlee@gmail.com>
2019-08-19 19:57:16 -0700

    Added Rotate Array Problem

---
+++ Rotate Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using VastTools;
+
+namespace Rotate_Array {
+  internal static class Program {
+    private static void Main(string[] args) {
+      int[] input = { 1, 2, 3, 4, 5, 6, 7 };
+      new Solution().Rotate(input, 3);
+      input.Log();
+    }
+  }
+}
---
+++ Rotate Array/Solution.cs
@@ -0,0 +1,19 @@
+﻿using VastTools;
+
+namespace Rotate_Array {
+  internal class Solution {
+    public void Rotate(int[] nums, int k) {
+      if(k < 1) { return; }
+
+      int swap;
+
+      for(int i = 0; i <= k; i++) {
+        swap = nums[0];
+        for(int i2 = 1; i2 < nums.Length; i2++) {
+          nums[i2 - 1] = nums[i2];
+        }
+        nums[nums.Length - 1] = swap;
+      }
+    }
+  }
+}
