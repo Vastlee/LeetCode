3106a950e8b4c72e0e5abd12eb2a34fbba9d1ba6
Vast <Vastlee@gmail.com>
2019-08-21 17:03:46 -0700

    Added Single Number with Solve

---
+++ Single Number/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Single_Number {
+  internal class Program {
+    private static void Main(string[] args) {
+      Console.WriteLine(new Solution().SingleNumber(new int[] { 4, 1, 2, 1, 2 }));
+    }
+  }
+}
---
+++ Single Number/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single_Number {
+  internal class Solution {
+    public int SingleNumber(int[] nums) {
+      var count = new Dictionary<int, int>();
+      for(int i = 0; i < nums.Length; i++) {
+        if(count.ContainsKey(nums[i])) {
+          count[nums[i]]++;
+        } else {
+          count.Add(nums[i], 1);
+        }
+      }
+
+      foreach(KeyValuePair<int, int> e in count) {
+        if(e.Value == 1) { return e.Key; }
+      }
+
+      return -1;
+    }
+  }
+}
