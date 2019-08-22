89532343aecce13e143e41f30d9b1c2b7de18984
Vast <Vastlee@gmail.com>
2019-08-22 07:45:06 -0700

    Added Solution for Best Time To Buy & Sell Stock II

--- Best Time to Buy and Sell Stock II/Solution.cs
+++ Best Time to Buy and Sell Stock II/Solution.cs
@@ -3,46 +3,15 @@
 namespace Best_Time_to_Buy_and_Sell_Stock_II {
   internal class Solution {
     public int MaxProfit(int[] prices) {
-      int indexOfLargest = FirstMinValueIndex(prices);
-      int len = prices.Length - 1;
+      int result = 0;
 
-      if(indexOfLargest == len) { return 0; }
-
-      int maxProfit = 0;
-      int largest;
-      int lastBuyPrice = prices[indexOfLargest];
-      int difference;
-
-      for(int i = indexOfLargest; i < len; i++) {
-        largest = 0;
-
-        for(int i2 = i + 1; i2 < prices.Length; i2++) {
-          if(prices[i2] <= prices[i]) { continue; }
-          difference = prices[i2] - prices[i];
-          Console.WriteLine($"{prices[i2]}-{prices[i]}={difference}");
-
-          if(difference > largest) {
-            largest = difference;
-            indexOfLargest = i2;
-          }
-        }
-        if(largest > 0) {
-          maxProfit += largest;
-          i = indexOfLargest;
+      for(int i = 1; i < prices.Length; i++) {
+        if(prices[i] > prices[i - 1]) {
+          result += prices[i] - prices[i - 1];
         }
       }
-      return maxProfit;
-    }
 
-    private int FirstMinValueIndex(int[] a) {
-      int lowest = a[0];
-      int indexOfLowest = 0;
-      for(int i = 1; i < a.Length; i++) {
-        if(a[i] < a[indexOfLowest]) {
-          indexOfLowest = i;
-        }
-      }
-      return indexOfLowest;
+      return result;
     }
   }
 }
