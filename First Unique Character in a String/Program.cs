8e65b612daf0551cfb87b1b59bb24a2f2daf787a
Vast <Vastlee@gmail.com>
2019-08-25 07:33:58 -0700

    Added Project: First Unique Character in a String

---
+++ First Unique Character in a String/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace First_Unique_Character_in_a_String {
+  internal static class Program {
+    private static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ First Unique Character in a String/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_Unique_Character_in_a_String {
+  internal class Solution {
+    public int FirstUniqChar(string s) {
+      int result = -1;
+
+      return result;
+    }
+  }
+}
