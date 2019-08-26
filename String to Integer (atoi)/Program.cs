05bfc41ceed7a60ca3b4a6d4eb1b728afc6e0b87
Vast <Vastlee@gmail.com>
2019-08-26 06:31:23 -0700

    Added Project String to Integer (atoi)

---
+++ String to Integer (atoi)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace String_to_Integer__atoi_ {
+  internal static class Program {
+    private static void Main(string[] args) {
+      var solve = new Solution();
+      string[] input = {
+        "42",
+        "   -42",
+        "4193 with words",
+        "words and 987",
+        "-91283472332"
+      };
+
+      foreach(string s in input) {
+        Console.WriteLine(solve.MyAtoi(s));
+      }
+    }
+  }
+}
---
+++ String to Integer (atoi)/Solution.cs
@@ -0,0 +1,7 @@
+﻿namespace String_to_Integer__atoi_ {
+  internal class Solution {
+    public int MyAtoi(string str) {
+      return -1;
+    }
+  }
+}
