4bcbac3bc78e23e3b605e92ffb3b3aa6f8789beb
Vast <Vastlee@gmail.com>
2019-08-25 17:20:54 -0700

    Added Solve for First Unique Character in a String

--- First Unique Character in a String/Program.cs
+++ First Unique Character in a String/Program.cs
@@ -3,7 +3,15 @@
 namespace First_Unique_Character_in_a_String {
   internal static class Program {
     private static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      var input = new string[] {
+        "leetcode",
+        "loveleetcode",
+        "",
+      };
+      var solve = new Solution();
+      foreach(string s in input) {
+        Console.WriteLine(solve.FirstUniqChar(s));
+      }
     }
   }
 }
--- First Unique Character in a String/Solution.cs
+++ First Unique Character in a String/Solution.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace First_Unique_Character_in_a_String {
   internal class Solution {
     public int FirstUniqChar(string s) {
-      int result = -1;
+      var unique = new Dictionary<char, IndexCountPair>();
 
-      return result;
+      for(int i = 0; i < s.Length; i++) {
+        if(unique.ContainsKey(s[i])) {
+          unique[s[i]].Count++;
+        } else {
+          unique.Add(s[i], new IndexCountPair(i, 1));
+        }
+      }
+
+      foreach(var pair in unique.Where(kv => kv.Value.Count == 1)) {
+        return pair.Value.Index;
+      }
+
+      return -1;
+    }
+
+    private class IndexCountPair {
+      public IndexCountPair(int index, int count) {
+        Index = index;
+        Count = count;
+      }
+
+      public int Index { get; private set; }
+      public int Count { get; set; }
     }
   }
 }
