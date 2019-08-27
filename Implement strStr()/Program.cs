2d89a49a0d30e9ca2e7955d2bc633e173c693827
Vast <Vastlee@gmail.com>
2019-08-27 11:39:24 -0700

    Added Project Implement strStr(), with Solution

---
+++ Implement strStr()/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Implement_strStr {
+  internal static class Program {
+    private static void Main(string[] args) {
+      var solve = new Solution();
+      string[][] input = {
+        new string[] {
+          "hello", "ll"
+        },
+        new string[] {
+          "aaaaa", "bba"
+        },
+        new string[] {
+          "a", "a"
+        },
+      };
+
+      foreach(string[] i in input) {
+        Console.WriteLine(solve.StrStr(i[0], i[1]));
+      }
+    }
+  }
+}
---
+++ Implement strStr()/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Implement_strStr {
+  internal class Solution {
+    public int StrStr(string haystack, string needle) {
+      if(needle == string.Empty) { return 0; }
+
+      for(int i = 0; i <= haystack.Length - needle.Length; i++) {
+        if(haystack.Substring(i, needle.Length).Equals(needle)) {
+          return i;
+        }
+      }
+
+      return -1;
+    }
+  }
+}
+
+/* Cheating...
+  public int StrStr(string haystack, string needle) {
+    return haystack.IndexOf(needle);
+  }
+ */
