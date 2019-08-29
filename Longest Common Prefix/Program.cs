c34fd9d4e233a56fbb99d2d748dff1600d690e72
Vast <Vastlee@gmail.com>
2019-08-29 07:00:20 -0700

    Added Project Longest Common Prefix

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Longest_Common_Prefix {
+  internal class Program {
+    private static void Main(string[] args) {
+      var solve = new Solution();
+      string[][] inputs = {
+        new string[] { "flower", "flow", "flight" },
+        new string[] { "dog", "racecar", "car" },
+      };
+
+      foreach(var input in inputs) {
+        Console.WriteLine(solve.LongestCommonPrefix(input));
+      }
+    }
+  }
+}
---
+++ Longest Common Prefix/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Longest_Common_Prefix {
+  internal class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+      if(strs == null || strs.Length < 1) { return string.Empty; }
+      string result = string.Empty;
+
+      foreach(var item in strs.OrderBy(x => x).ToArray()) {
+        Console.Write($"{item}, ");
+      }
+
+      return result;
+    }
+  }
+}
