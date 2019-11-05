23b37306014def5247dd6825b9ee001b00221160
Vast <Vastlee@gmail.com>
2019-11-05 08:06:13 -0800

    Added Project Bulls & Cows with Solution

---
+++ Bulls and Cows/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Solution {
+    public string GetHint(string secret, string guess) {
+        int bulls, cows;
+        var secretArr = secret.ToCharArray();
+        var guessArr = guess.ToCharArray();
+
+        bulls = CountAndMarkBulls(secretArr, guessArr);
+        cows = CountAndMarkCows(secretArr, guessArr);
+        return $"{bulls}A{cows}B";
+    }
+
+    private int CountAndMarkBulls(char[] secret, char[] guess) {
+        int result = 0;
+        for(int i = 0; i < secret.Length; i++) {
+            if(secret[i] == guess[i]) {
+                result++;
+                secret[i] = ' ';
+                guess[i] = ' ';
+            }
+        }
+        return result;
+    }
+
+    private int CountAndMarkCows(char[] secret, char[] guess) {
+        int result = 0;
+        for(int i = 0; i < secret.Length; i++) {
+            if(guess[i] == ' ') { continue; }
+            if(FindAndMarkCow(secret, guess[i])) {
+                guess[i] = ' ';
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private bool FindAndMarkCow(char[] secret, char guess) {
+        for(int i = 0; i < secret.Length; i++) {
+            if(secret[i] == ' ') { continue; }
+            if(secret[i] == guess) {
+                    secret[i] = ' ';
+                    return true;
+            }
+        }
+        return false;
+    }
+}
--- Longest Common Prefix/Solution.cs
+++ Longest Common Prefix/Solution.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -8,11 +7,41 @@ namespace Longest_Common_Prefix {
     public string LongestCommonPrefix(string[] strs) {
       if(strs == null || strs.Length < 1) { return string.Empty; }
       var result = new StringBuilder();
-
       var skip = new HashSet<int>();
       int longest = strs.OrderBy(x => x.Length).First().Length;
+      int hits = -1;
+      Dictionary<char, List<int>> charHits;
 
-      for(int i = 0; i < longest; i++) {
+      for(int c = 0; c < longest || hits == 0; c++) {
+        hits = 0;
+        charHits = new Dictionary<char, List<int>>();
+
+        for(int i = 0; i < strs.Length; i++) {
+          if(skip.Contains(i)) { continue; }
+          if(i >= strs[i].Length && !skip.Contains(i)) {
+            skip.Add(i);
+            continue;
+          }
+
+          if(charHits.ContainsKey(strs[i][c])) {
+            charHits[strs[i][c]].Add(i);
+          } else {
+            charHits.Add(strs[i][c], new List<int>() { i });
+          }
+        }
+
+        var matchLines = charHits
+          .Where(x => x.Value.Count == 1)
+          .Select(x => x.Value)
+          .SelectMany(x => x);
+
+        hits = matchLines.Count();
+        if(hits < 2) {
+        }
+
+        foreach(int line in matchLines) {
+          skip.Add(line);
+        }
       }
 
       return result.ToString();
