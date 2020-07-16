668ae96bb7f02d720a29c257af850d2b1c4f0154
Vast <Vastlee@gmail.com>
2020-07-16 08:19:28 -0700

    Added solution for Reverse Linked List

---
+++ Reverse Linked List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        var solve = new Solution();
+        ListNode head = new ListNode(0);
+        ListNode cNode = head;
+
+        for(int i = cNode.val + 1; i <= 5; i++) {
+            cNode.next = new ListNode(i);
+            cNode = cNode.next;
+        }
+
+        head = solve.ReverseList(head);
+
+        while(head != null) {
+            Console.WriteLine($"{head.val}");
+            head = head.next;
+        }
+    }
+}
