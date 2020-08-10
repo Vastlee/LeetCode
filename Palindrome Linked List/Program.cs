6c575fda924a40aa5657542805d480a9e03798bc
Vast <Vastlee@gmail.com>
2020-08-10 16:47:42 -0700

    Added some Tests and CodeMaid

--- Palindrome Linked List/Program.cs
+++ Palindrome Linked List/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using LeetCodeHelper;
 
 namespace Palindrome_Linked_List {
-    class Program {
-        static void Main(string[] args) {
-            ListNode l1 = HelperMethods.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
-            ListNode l2 = HelperMethods.CreateLinkedList(new int[] { 5, 4, 3, 2, 1 });
+    internal class Program {
+        private static void Main(string[] args) {
+            ListNode l1 = HelperMethods.CreateLinkedList(new int[] { 1, 2, 3, 2, 1 });
+            ListNode l2 = HelperMethods.CreateLinkedList(new int[] { 1, 9, 8, 8 });
+            ListNode l3 = HelperMethods.CreateLinkedList(new int[] { 1 });
+            ListNode l4 = HelperMethods.CreateLinkedList(new int[] { });
 
+            var solve = new Solution();
 
+            Console.WriteLine(solve.IsPalindrome(l1));
+            Console.WriteLine(solve.IsPalindrome(l2));
+            Console.WriteLine(solve.IsPalindrome(l3));
+            Console.WriteLine(solve.IsPalindrome(l4));
+        }
+
+        static private IEnumerable<ListNode> Nodes(ListNode head) {
+            while(head != null) {
+                yield return head;
+                head = head.next;
+            }
         }
     }
 }
--- Palindrome Linked List/Solution.cs
+++ Palindrome Linked List/Solution.cs
@@ -3,21 +3,31 @@
 namespace Palindrome_Linked_List {
     internal class Solution {
         public bool IsPalindrome(ListNode head) {
-            return false;
+            if(head == null || head.next == null) { return true; }
+
+            ListNode reversed = Reverse(head);
+
+            while(head != null) {
+                if(head.val != reversed.val) { return false; }
+                head = head.next;
+                reversed = reversed.next;
+            }
+
+            return true;
         }
 
         private ListNode Reverse(ListNode head) {
-            ListNode previous, next;
+            ListNode reversed, previous;
+            reversed = previous = null;
 
-            while(head.next != null) {
-                previous = head;
+            while(head != null) {
+                previous = reversed;
+                reversed = new ListNode(head.val);
+                reversed.next = previous;
                 head = head.next;
-                next = head.next;
-                head.next = previous;
-                head = next;
             }
 
-            return head;
+            return reversed;
         }
     }
 }
