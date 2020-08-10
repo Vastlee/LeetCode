5cb8d3f5e87a54bc54d4b623f42854bf1776707e
Vast <Vastlee@gmail.com>
2020-08-10 11:18:06 -0700

    Setup New Project for Palindrome Linked List

---
+++ Palindrome Linked List/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LeetCodeHelper;
+
+namespace Palindrome_Linked_List {
+    class Program {
+        static void Main(string[] args) {
+            ListNode l1 = HelperMethods.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            ListNode l2 = HelperMethods.CreateLinkedList(new int[] { 5, 4, 3, 2, 1 });
+
+
+        }
+    }
+}
---
+++ Palindrome Linked List/Solution.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeHelper;
+
+namespace Palindrome_Linked_List {
+    internal class Solution {
+        public bool IsPalindrome(ListNode head) {
+            return false;
+        }
+
+        private ListNode Reverse(ListNode head) {
+            ListNode previous, next;
+
+            while(head.next != null) {
+                previous = head;
+                head = head.next;
+                next = head.next;
+                head.next = previous;
+                head = next;
+            }
+
+            return head;
+        }
+    }
+}
