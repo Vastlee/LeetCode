202b23b08611e8d992bf3a512b9d981db9447eb0
Vast <Vastlee@gmail.com>
2020-08-10 17:51:34 -0700

    Added Solution for Linked List Cycle

--- Linked List Cycle/Program.cs
+++ Linked List Cycle/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using LeetCodeHelper;
 
 namespace Linked_List_Cycle {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            ListNode l1 = HelperMethods.CreateLinkedList(new int[] { 1, 2, 3, 2, 1 });
+            ListNode l2 = HelperMethods.CreateLinkedList(new int[] { 1, 9, 8, 8 });
+            ListNode l3 = HelperMethods.CreateLinkedList(new int[] { 1 });
+            ListNode l4 = HelperMethods.CreateLinkedList(new int[] { });
+
+            var solve = new Solution();
+
+            Console.WriteLine(solve.HasCycle(l1));
         }
     }
 }
--- Linked List Cycle/Solution.cs
+++ Linked List Cycle/Solution.cs
@@ -1,4 +1,18 @@
-﻿namespace Linked_List_Cycle {
-    class Solution {
+﻿using System.Collections.Generic;
+using LeetCodeHelper;
+
+namespace Linked_List_Cycle {
+    internal class Solution {
+        public bool HasCycle(ListNode head) {
+            var map = new HashSet<ListNode>();
+
+            while(head != null) {
+                if(map.Contains(head)) { return true; }
+                map.Add(head);
+                head = head.next;
+            }
+
+            return false;
+        }
     }
 }
